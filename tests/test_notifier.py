298a7e7225e22318109ef0d408b9776038f8f2dd
Matěj Grabovský <matej@mgrabovsky.net>
2022-01-02 20:18:19 +0100

    tests: Add tests and run them on code check

---
+++ tests/test_notifier.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+from sms.notifier import generate_hash, is_modified
+
+
+class IsModifiedTestCase(TestCase):
+    def test_empty(self) -> None:
+        empty_blob = b''
+        empty_hash = generate_hash(empty_blob)
+        self.assertFalse(is_modified(empty_blob, empty_hash))
+
+    def test_non_empty(self) -> None:
+        empty_blob = b''
+        empty_hash = generate_hash(empty_blob)
+        self.assertTrue(is_modified(b'non empty text', empty_hash))
